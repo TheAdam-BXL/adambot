ea5fe10701efc6e97321f27c2d95086cf3926541
TheAdam-BXL <46106141+TheAdam-BXL@users.noreply.github.com>
2018-12-24 13:57:51 +0100

    Add files via upload

---
+++ files/img/gifs/triggeredinvert.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+let botconfig = require("../botconfig.json");
+let misc = require("../misc.json");
+const client = new Discord.Client({disableEveryone: true});
+const lang = require('i18n')
+const arcadia = require('arcadia-module');
+const snekfetch = require('snekfetch');
+//const superagent = require("superagent");
+
+var prefix = (botconfig.prefix)
+
+
+module.exports.run = async (bot, message, args) => {
+    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.channel.type === "dm") return;
+    if(message.author.bot) return;
+
+        let member = message.mentions.members.first() || message.member;
+        message.channel.send("INFO >> Loading...").then (async msg => {
+        await message.channel.send({
+          files: [{
+            attachment: `https://www.arcadia-api.xyz/api/v1/triggeredinvert?url=${member.user.displayAvatarURL}&type=1`,
+            name: `${member.user.username}-triggeredinvert.gif`
+          }]
+          
+        })
+        await msg.delete()
+    })
+}
+
+module.exports.help = {
+    name: "triggeredinvert"
+}
